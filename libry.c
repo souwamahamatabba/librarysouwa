1493f3b09c11e7dedca451c6eb96c605885eea6c
souwamahamatabba <souwamahamatabba@gmail.com>
2025-01-23 23:43:09 +0000

    Update libry.c

--- libry.c
+++ libry.c
@@ -1,21 +1,65 @@
-#include "libry.h" 
-// DÈfinition d'une fonction de calcul de factorielle. 
-unsigned int fact( unsigned int value ) { 
-unsigned int result = 1; 
-while ( value > 1 ) { 
-result *= value; 
-value --; 
-} 
-return result; 
-} 
-// DÈfinition d'une fonction d'ÈlÈvation ‡ une puissance donnÈes. 
-int power( int value, unsigned int pow ) { 
-if ( pow == 0 ) return 1; 
-if ( pow == 1 ) return value; 
-int accumulator = 1; 
-while( pow > 0 ) { 
-accumulator *= value; 
-pow--; 
-} 
-return accumulator; 
-} 
+#include <stdio.h>
+#include "libry.h"
+// Exercice 1 : Afficher "Bonjour"
+void afficherBonjour() {
+    printf("Bonjour\n");
+}
+
+// Exercice 2 : Inverser un entier √† trois chiffres
+int inverserEntier(int n) {
+    int centaine = n / 100;
+    int dizaine = (n / 10) % 10;
+    int unite = n % 10;
+    return unite * 100 + dizaine * 10 + centaine;
+}
+
+// Exercice 3 : Produit de deux nombres r√©els
+float produitDeuxReels(float a, float b) {
+    return a * b;
+}
+
+// Exercice 4 : V√©rifier si un entier est pair ou impair
+int estPair(int n) {
+    return n % 2 == 0;
+}
+
+// Exercice 5 : Comparer deux entiers
+const char* comparerEntiers(int a, int b) {
+    if (a == b) return "a = b";
+    else if (a > b) return "a > b";
+    else return "a < b";
+}
+
+// Exercice 6 : Trier trois entiers
+void trierTroisEntiers(int *a, int *b, int *c) {
+    if (*a > *b) { int temp = *a; *a = *b; *b = temp; }
+    if (*a > *c) { int temp = *a; *a = *c; *c = temp; }
+    if (*b > *c) { int temp = *b; *b = *c; *c = temp; }
+}
+
+// Exercice 7 : Jeu de devinette
+void devinerNombre() {
+    int nombreSecret = 42; // Nombre pr√©d√©fini
+    int proposition;
+    printf("Devinez le nombre (entre 0 et 100) : ");
+    do {
+        scanf("%d", &proposition);
+        if (proposition < nombreSecret)
+            printf("le nombre a devin√© est plus grand\n");
+        else if (proposition > nombreSecret)
+            printf("le nombre a devin√© est plus petit\n");
+        else
+            printf("F√©licitations, vous avez devin√© le nombre !\n");
+    } while (proposition != nombreSecret);
+}
+
+// Exercice 16 : Permutation
+void permuterChar(char *a, char *b) {
+    char temp = *a;
+    *a = *b;
+    *b = temp;
+}
+// Exercice 17 : Somme
+int somme(int a, int b) {
+    return a + b;
+}
