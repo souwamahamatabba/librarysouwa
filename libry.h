35456ce099406e862c798436e7084909adbbedb4
souwamahamatabba <souwamahamatabba@gmail.com>
2025-01-23 23:41:58 +0000

    Update libry.h

--- libry.h
+++ libry.h
@@ -1,7 +1,13 @@
-#ifndef __MY_MATH_LIB_H 
-#define __MY_MATH_LIB_H 
-// Définition d'une fonction de calcul de factorielle. 
-unsigned int fact( unsigned int value ); 
-// Définition d'une fonction d'élévation à une puissance données. 
-int power( int value, unsigned int pow ); 
+#ifndef LIBRY_H
+#define LIBRY_H
+void afficherBonjour();
+int inverserEntier(int n);
+float produitDeuxReels(float a, float b);
+int estPair(int n);
+const char* comparerEntiers(int a, int b);
+void trierTroisEntiers(int *a, int *b, int *c);
+void devinerNombre();
+void permuterChar(char *a, char *b);
+int somme(int a, int b);
+
 #endif
